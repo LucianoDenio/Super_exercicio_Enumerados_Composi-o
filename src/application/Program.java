ce76d3082434f96825cea848ca1ae92b090cd533
Luciano Denio <lucianodenio@gmail.com>
2022-11-10 22:35:49 -0300

    implementando programa principal e logicas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,62 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Client;
+import entities.OrderItem;
+import entities.Product;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		SimpleDateFormat fmt1 = new SimpleDateFormat("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = new DateTimeFormatter("dd/MM/yyyy HH:mm:ss").withZone(ZoneId.systemDefault());
 		
+		System.out.println("Enter Cliente Data: ");
+		System.out.print("Name: ");
+		sc.nextLine();
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		sc.nextLine();
+		String email = sc.nextLine();
+		System.out.print("BirtH Date (DD/MM/YYYY) :");
+		Date birthDate = fmt1.parse(sc.next());
+		
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		String status = sc.next();
+		LocalDateTime moment = LocalDateTime.now();
+		
+		System.out.print("How many items to this order: ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			System.out.println("Enter #"+i+" item data: ");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			System.out.print("Quatity: ");
+			int quantity = sc.nextInt();
+			
+			Product product = new Product(productName, productPrice);
+			OrderItem item = new OrderItem( quantity, product.getPrice());
+		}
+		
+		System.out.println();
 		
 		
 		
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -12,5 +12,47 @@ public class Order {
 	private OrderStatus status;
 	
 	private List<OrderItem> cart = new ArrayList<>();
+	
+	public Order() {
+	}
+	
+	public Order (Date moment, OrderStatus status) {
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public void addItem(OrderItem item ) {
+		cart.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		cart.remove(item);
+	}
+	
+	public Double total() {
+		double total = 0;
+		for(OrderItem c : cart) {
+			total += c.subTotal();		
+		}
+		return total;
+	}
+	
+	
 
 }
